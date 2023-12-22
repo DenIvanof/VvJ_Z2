c87b5ca9be77228ea0993e3c84d9fbe06c07a5b9
Ivanich <deniska090798@gmail.com>
2023-12-22 20:30:59 +0400

    add

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,17 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Пополнение баланса мобильного телефона");
-        int x = 100;
-        System.out.println("Начальный баланс " + (x) + "руб");
-        int y;
-        y = 1100;
-        System.out.println("Вы внесли на баланс " + (y) + "руб");
-        int z;
-        if (y <= 1000) {
-            z = (x) + (y);
+        int balance = 100;
+        System.out.println("Начальный баланс " + (balance) + "руб");
+        int money;
+        money = 1100;
+        System.out.println("Вы внесли на баланс " + (money) + "руб");
+        int result;
+        if (money <= 1000) {
+            result = (balance) + (money);
         } else {
-            z = (x) + ((y) + ((y) / 100));
+            result = (balance) + ((money) + ((money) / 100));
         }
-        System.out.println("Ваш текущий баланс составляет " + (z) + "руб");
+        System.out.println("Ваш текущий баланс составляет " + (result) + "руб");
     }
 }
